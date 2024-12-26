c578404788d8fcddb4b83d88265c860ce1ba02d6
satyajay01 <satyajaydibya0@gmail.com>
2024-12-26 15:02:40 +0530

    Contact page change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,124 +1,107 @@
-import React, { useEffect, useState } from 'react'
-import './Contact.scss'
-import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faWhatsapp,
-  faLinkedin,
-  faGithub,
-} from '@fortawesome/free-brands-svg-icons'
+import React, { useEffect, useState } from 'react';
+import './Contact.scss';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
-
+  const [letterClass, setLetterClass] = useState('text-animate');
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 3000)
-    return () => clearTimeout(timer)
-  }, [])
+      setLetterClass('text-animate-hover');
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendMail = (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    const data = {
+      to_name: name,
+      to_email: email,
+      message,
+    };
+    const serviceId = 'satyajay_email_serviceId';
+    const templateId = 'Satyajay_templateid';
+    const userId = 'yefBktnctXaY8sbkM';
+
+    emailjs.send(serviceId, templateId, data, userId).then(
+      (response) => {
+        alert('Message sent successfully!');
+        console.log('Success:', response);
+        setName('');
+        setEmail('');
+        setMessage('');
+      },
+      (error) => {
+        alert('Failed to send message. Please try again.');
+        console.log('Error:', error);
+      }
+    );
+  };
+
   return (
-    <>
-      <div className="container contact-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e','.']}
-              idx={15}
-            />
-          </h1>
-          <p>
-            I am particularly interested in Web app project development.
-            However, if you have any other requests or questions, feel free to
-            contact me using the links below.
-          </p>
-          <div className="contact-form">
-          {/* Links */}
-          <div className='contact-link'>
-                <ul>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href="https://wa.me/917024993010"
-                    >
-                      <FontAwesomeIcon icon={faWhatsapp} color="green" /> <span> WhatsApp</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href="mailto:satyajaydibya9@gmail.com"
-                    >
-                      <FontAwesomeIcon icon={faEnvelope} color="rgb(191, 109, 54)" /> <span>Email</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href="https://www.linkedin.com/in/satyajay01"
-                    >
-                      <FontAwesomeIcon icon={faLinkedin} color="rgb(20, 105, 199)" /> <span>Linkedin</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href="https://github.com/Satyajay01"
-                    >
-                      <FontAwesomeIcon icon={faGithub} color="#000000" /> <span>Github</span>
-                    </a>
-                  </li>
-                </ul>
-          </div>
-            {/* <form>
-              <ul>
-                <li className="half">
-                  <input type="text" name="" placeholder="Name" required />
-                </li>
-                <li className="half">
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                  />
-                </li>
-                <li>
-                  <input
-                    type="text"
-                    name="subject"
-                    placeholder="Name"
-                    required
-                  />
-                </li>
-                <li>
-                  <textarea
-                    placeholder="Message"
-                    name="message"
-                    required
-                  ></textarea>
-                </li>
-                <li>
-                  <input
-                    type="submit"
-                    className="flat-button"
-                    value="SEND"
-                  ></input>
-                </li>
-              </ul>
-            </form> */}
-          </div>
+    <div className="container contact-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e', '.']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          I am specifically interested in web app project development and I
+          need an internship or job. However, if you have any other requests or
+          questions, please feel free to contact me using the form below.
+        </p>
+        <div className="contact-form">
+          <form onSubmit={sendMail}>
+            <ul>
+              <li className="half">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </li>
+              <li className="half">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </li>
+              <li>
+                <textarea
+                  placeholder="Message"
+                  name="message"
+                  required
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+              </li>
+              <li>
+                <button type="submit" className="flat-button">
+                  SEND
+                </button>
+              </li>
+            </ul>
+          </form>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
