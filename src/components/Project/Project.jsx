41f6edcad79c850a2e1ec6af1699203dbb9b967a
satyajay01 <satyajaydibya0@gmail.com>
2024-12-28 13:50:14 +0530

    Project page responsive

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -12,9 +12,9 @@ const Project = () => {
     return () => clearTimeout(timer)
   }, [])
 
-  // project video 
-  const videoId1 = "hSBhJjST2vs";
-  const videoId2 = "fIUk4rcQbnc";
+  // project video
+  const videoId1 = 'hSBhJjST2vs'
+  const videoId2 = 'fIUk4rcQbnc'
   return (
     <div>
       <div className="project-container">
@@ -26,52 +26,57 @@ const Project = () => {
           />
         </h1>
         <br /> <br />
-        <h3 className="project-heading">
-          I have created many projects to enhance my skills which are available
-          in my <a href="https://github.com/Satyajay01">GitHub</a> profile
-          <br />
-          and sample of some projects are given below.
-        </h3>
-        <div className="project">
-        <iframe
-        src={`https://www.youtube.com/embed/${videoId1}?autoplay=1&loop=1&playlist=${videoId1}&vq=hd700`}
-        frameBorder="0"
-        allow="accelerometer; autoplay;"
-        allowFullScreen
-      ></iframe>
-          <div>
-            <h3>BookStore— Full-Stack (MERN) Project</h3>
-            <a href="https://github.com/Satyajay01/BookStore-MERN">
-              https://github.com/Satyajay01/BookStore-MERN
-            </a>
+        <div className="project-border">
+          <h3 className="project-heading">
+            I have created many projects to enhance my skills which are
+            available in my <a href="https://github.com/Satyajay01">GitHub</a>{' '}
+            profile
             <br />
-            ● The Bookstore project aims to provide an online book-shop platform
-            where users can purchase and download books. <br /> ● The front-end
-            of the project is built using React.js, Tailwind CSS, and DaisyUI
-            allowing the design to enhance user experience with fast and
-            responsive interactions. <br /> ● The back-end, built with Node.js,
-            Express, and MongoDB, eciently manages user authentication, book
-            inventory, and order processing.
+            and sample of some projects are given below.
+          </h3>
+          <div className="project">
+            <iframe
+              className="project-video"
+              src={`https://www.youtube.com/embed/${videoId1}?autoplay=1&loop=1&playlist=${videoId1}&vq=hd700`}
+              frameBorder="0"
+              allow="accelerometer; autoplay;"
+              allowFullScreen
+            ></iframe>
+            <div>
+              <h3>(1) BookStore— Full-Stack (MERN) Project</h3>
+              <a href="https://github.com/Satyajay01/BookStore-MERN">
+                https://github.com/Satyajay01/BookStore-MERN
+              </a>
+              <br />
+              ● The Bookstore project aims to provide an online book-shop
+              platform where users can purchase and download books. <br /> ● The
+              front-end of the project is built using React.js, Tailwind CSS,
+              and DaisyUI allowing the design to enhance user experience with
+              fast and responsive interactions. <br /> ● The back-end, built
+              with Node.js, Express, and MongoDB, eciently manages user
+              authentication, book inventory, and order processing.
+            </div>
           </div>
-        </div>{' '}
-        <br /> <br />
-        <div className="project">
-        <iframe
-        src={`https://www.youtube.com/embed/${videoId2}?autoplay=1&loop=1&playlist=${videoId2}&vq=hd700`}
-        frameBorder="0"
-        allow="accelerometer; autoplay;"
-        allowFullScreen
-      ></iframe>
-          <div>
-            <h3>Ochi— Animated home page clone</h3>
-            <a href="https://github.com/Satyajay01/OCHI">
-            https://github.com/Satyajay01/OCHI
-            </a>
-            <br />● This project is a clone of ochi.design website which I
-            created with the help of React.js. <br />● To make the WebPage look good
-            and attractive, Tailwind, which is a CSS framework, has been used. <br /> ●
-            and in this project we have used Framer Motion, which makes the
-            WebPage smooth and applies the animation.
+          <br /> <br />
+          <div className="project">
+            <iframe
+              className="project-video"
+              src={`https://www.youtube.com/embed/${videoId2}?autoplay=1&loop=1&playlist=${videoId2}&vq=hd700`}
+              frameBorder="0"
+              allow="accelerometer; autoplay;"
+              allowFullScreen
+            ></iframe>
+            <div>
+              <h3>(2) Ochi— Animated home page clone</h3>
+              <a href="https://github.com/Satyajay01/OCHI">
+                https://github.com/Satyajay01/OCHI
+              </a>
+              <br />● This project is a clone of ochi.design website which I
+              created with the help of React.js. <br />● To make the WebPage
+              look good and attractive, Tailwind, which is a CSS framework, has
+              been used. <br /> ● and in this project we have used Framer
+              Motion, which makes the WebPage smooth and applies the animation.
+            </div>
           </div>
         </div>
       </div>
