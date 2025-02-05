9e181aeeeb59dc386348f4245a37da4f9aa4aa94
satyajay01 <satyajaydibya0@gmail.com>
2025-02-05 16:39:10 +0530

    Skills update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ import {
   faJsSquare,
   faNodeJs,
   faReact,
-  faGitAlt,
+  faPhp,
 } from '@fortawesome/free-brands-svg-icons'
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import { faBoxes } from '@fortawesome/free-solid-svg-icons'
@@ -42,8 +42,9 @@ const About = () => {
             <span className="skill-text">
               Bachelor of Computer Applications
             </span>
-            and also pursuing MSc Computer Science, and I have completed my <span  className="skill-text">Full
-            Stack Developer course</span>, I also have
+            and also pursuing MSc Computer Science, and I have completed my{' '}
+            <span className="skill-text">Full Stack Developer course</span>, I
+            also have
             <span className="skill-text">1 year of experience</span> as a
             <span className="skill-text">Computer Technician.</span>
           </p>
@@ -54,14 +55,6 @@ const About = () => {
               idx={10}
             />
           </h1>
-          <h3> Computer Technician:</h3>
-          <span className="skill-text"> Technical Support </span>
-          <span className="skill-text"> Remote Access Software </span>
-          <span className="skill-text"> IT </span>
-          <span className="skill-text"> Help Desktop Support </span>
-          <span className="skill-text"> Software Troubleshooting</span>
-          <span className="skill-text"> Operating system </span>
-          <span className="skill-text"> Hardware </span>
           <h3> Full-Stack Development:</h3>
           <span className="skill-text"> HTML </span>
           <span className="skill-text"> CSS </span>
@@ -70,57 +63,76 @@ const About = () => {
           <span className="skill-text"> Tailwind CSS</span>
           <span className="skill-text"> Daisy UI </span>
           <span className="skill-text"> React.js </span>
+          <span className="skill-text"> PHP </span>
           <span className="skill-text"> Node.js </span>
           <span className="skill-text"> Express.js </span>
           <span className="skill-text"> MongoDB </span>
           <span className="skill-text"> MySql</span>
           <span className="skill-text"> NoSql</span>
+          <span className="skill-text"> Database</span>
           <span className="skill-text"> Rest API</span>
           <span className="skill-text"> JSON </span>
+          <span className="skill-text"> OOP </span>
           <span className="skill-text"> C/C++ </span>
           <span className="skill-text"> Git/Github </span>
-          <span className="skill-text"> AI </span>
           <span className="skill-text"> ChatGPT </span>
           <span className="skill-text"> Postman </span>
           <span className="skill-text"> Vs Code </span>
+          <h3> Computer Technician:</h3>
+          <span className="skill-text"> Technical Support </span>
+          <span className="skill-text"> Remote Access Software </span>
+          <span className="skill-text"> IT </span>
+          <span className="skill-text"> Help Desktop Support </span>
+          <span className="skill-text"> Software Troubleshooting</span>
+          <span className="skill-text"> Operating system </span>
+          <span className="skill-text"> Hardware </span>
         </div>
       </div>
 
-    {/* ---------cube----------- */}
+      {/* ---------cube----------- */}
       <div className="cube-box">
-        <div className='cube-contener'>
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon
-              icon={faBoxes}
-              color="#38BDF8"
-              title="Tailwind CSS"
-            />
+        <div className="cube-contener">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon
+                icon={faBoxes}
+                color="#38BDF8"
+                title="Tailwind CSS"
+              />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon
+                icon={faJsSquare}
+                color="#F7DF1E"
+                title="JavaScript"
+              />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faReact} color="#61DAFB" title="React" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon
+                icon={faNodeJs}
+                color="#68A063"
+                title="Node.js"
+              />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon
+                icon={faDatabase}
+                color="#47A248"
+                title="MongoDB"
+              />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon
+                icon={faPhp}
+                color="#5d629e"
+                title="PHP"
+                transform={{ rotate: -90 }}
+              />
+            </div>
           </div>
-          <div className="face2">
-            <FontAwesomeIcon
-              icon={faJsSquare}
-              color="#F7DF1E"
-              title="JavaScript"
-            />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faReact} color="#61DAFB" title="React" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faNodeJs} color="#68A063" title="Node.js" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon
-              icon={faDatabase}
-              color="#47A248"
-              title="MongoDB"
-            />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#F1502F" title="Git" />
-          </div>
-        </div>
         </div>
       </div>
     </div>
